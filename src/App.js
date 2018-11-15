e1ae66fdd81576043b0393f0bd7bd7bd4210ab2f
aharxict <it.ahapx@gmail.com>
2018-11-15 14:10:02 +0200

    added inline style and ext style file

--- src/App.js
+++ src/App.js
@@ -19,17 +19,34 @@ class App extends Component {
       ]
     } )
   };
-
+  nameChangeHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: 'CCC', age: 25 },
+        { name: event.target.value, age: 22 },
+        { name: 'DDD1', age: 31 }
+      ]
+    } )
+  };
   render() {
+    const style = {
+      color: 'red',
+      padding: '8px',
+      border: '1px solid blue',
+      backgroundColor: 'transparent',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
         <h1>Title</h1>
-        <button onClick={ () => this.changeNameHandler('Body') }>Switch me</button>
+        <button style={style}
+          onClick={ () => this.changeNameHandler('Body') }>Switch me</button>
         <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
         <Person
           name = {this.state.persons[1].name}
           age = {this.state.persons[1].age}
-          click = {this.changeNameHandler.bind(this, 'DDD')} > Inner text <b>bold</b> </Person>
+          click = {this.changeNameHandler.bind(this, 'DDD')}
+          changed = {this.nameChangeHandler}> Inner text <b>bold</b> </Person>
         <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import './Person.css'
 const person = (props) => {
     return (
-        <div>
+        <div className='Person'>
           <p>Hello {props.name} world with {props.age}! </p>
           <p>{props.children}</p>
           <button onClick={props.click}>Inner BTN</button>
+          <input onChange={props.changed} type="text" value={props.name}/>
         </div>
     )
 };
